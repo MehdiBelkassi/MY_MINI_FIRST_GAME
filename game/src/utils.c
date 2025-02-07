4c81f431518a4251070b3e7ac821fbbf96950692
Mehdi Belkassi <mehdibelkassi1@gmail.com>
2025-02-07 17:04:16 +0100

    Delete game directory and adding a better version

--- game/src/utils.c
+++
@@ -1,9 +0,0 @@
-#include "so_long.h"
-
-void free_map(char **map, int map_height) {
-    if (map) {
-        for (int i = 0; i < map_height; i++)
-            free(map[i]);
-        free(map);
-    }
-}
