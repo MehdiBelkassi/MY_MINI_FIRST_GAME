ecb910df2251b9d6e3d8dd6fbef893e2e2ac2bef
Mehdi Belkassi <mehdibelkassi1@gmail.com>
2025-03-07 07:59:54 +0000

    Add files via upload

---
+++ aben/alright/srcb/game_bonus.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbelkass <mbelkass@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 06:29:14 by mbelkass          #+#    #+#             */
+/*   Updated: 2025/03/07 07:57:52 by mbelkass         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+int	close_window(t_game *game)
+{
+	mlx_destroy_window(game->mlx, game->win);
+	mlx_destroy_image(game->mlx, game->wall_img);
+	mlx_destroy_image(game->mlx, game->space_img);
+	mlx_destroy_image(game->mlx, game->collectible_img);
+	mlx_destroy_image(game->mlx, game->exit_img);
+	mlx_destroy_image(game->mlx, game->player_img);
+	mlx_destroy_image(game->mlx, game->enemy_img);
+	free_map(game->map, game->map_height);
+	if (game->moves_str)
+		free(game->moves_str);
+	exit(0);
+	return (0);
+}
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (s[i]) 
+	{
+		j = 0;
+		while (reject[j])
+		{
+			if (s[i] == reject[j]) 
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
